180b14f590cbbdbc522cdf01c8c37d5241866cca
Yury Glushkov <mail@glushkov.us>
2020-10-28 15:28:23 +0300

    Solution structure changed

---
+++ tests/VASPSuite.EtherGate.BehaviorTests/Support/DependencyInjection.cs
@@ -0,0 +1,47 @@
+using Autofac;
+using JetBrains.Annotations;
+using Nethereum.Web3;
+using SpecFlow.Autofac;
+using VASPSuite.EtherGate.BehaviorTests.Support.SmartContracts;
+using VASPSuite.EtherGate.BehaviorTests.Support.StepDefinitions;
+
+namespace VASPSuite.EtherGate.BehaviorTests.Support
+{
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public static class DependencyInjection
+    {
+        [ScenarioDependencies]
+        public static ContainerBuilder CreateContainerBuilder()
+        {
+            var builder = new ContainerBuilder();
+
+            builder
+                .RegisterType<Accounts>()
+                .SingleInstance();
+            
+            builder
+                .RegisterType<SmartContractDeployer>()
+                .SingleInstance();
+            
+            builder
+                .RegisterType<Givens>()
+                .SingleInstance();
+            
+            builder
+                .RegisterType<Whens>()
+                .SingleInstance();
+
+            builder
+                .RegisterType<Thens>()
+                .SingleInstance();
+            
+            builder
+                .RegisterType<Web3>()
+                .As<IWeb3>()
+                .SingleInstance();
+
+            
+            return builder;
+        }
+    }
+}
